70b3101a47bf4a79972c71c9d4d386a0fa27ee4e
Charles Stafford <cstaff@Charless-MacBook-Pro.local>
2016-09-13 10:35:58 -0600

    removed unnecessary img

--- snakeTracker/www/js/controllers.js
+++ snakeTracker/www/js/controllers.js
@@ -4,32 +4,36 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {
 
-  $scope.scan = function() {
-  cordova.plugins.barcodeScanner.scan(
-    function(result) {
-      if (!result.cancelled) {
-        if (result.format == "QR_CODE") {
-          navigator.notification.prompt("Please enter name of data", function(input) {
-            var name = input.input1;
-            var value = result.text;
+//   $scope.scan = function() {
+//   cordova.plugins.barcodeScanner.scan(
+//     function(result) {
+//       if (!result.cancelled) {
+//         if (result.format == "QR_CODE") {
+//           navigator.notification.prompt("Please enter name of data", function(input) {
+//             var name = input.input1;
+//             var value = result.text;
+//
+//             var data = localStorage.getItem("LocalData");
+//             console.log(data);
+//             data = JSON.parse(data);
+//             data[data.length] = [name, value];
+//
+//             localStorage.setItem("LocalData", JSON.stringify(data));
+//
+//             alert("Done");
+//           });
+//         }
+//       }
+//     },
+//     function(error) {
+//       alert("Scanning failed: " + error);
+//     }
+//   );
+// };
 
-            var data = localStorage.getItem("LocalData");
-            console.log(data);
-            data = JSON.parse(data);
-            data[data.length] = [name, value];
+  var vm = this;
 
-            localStorage.setItem("LocalData", JSON.stringify(data));
-
-            alert("Done");
-          });
-        }
-      }
-    },
-    function(error) {
-      alert("Scanning failed: " + error);
-    }
-  );
-}
+  vm.message = "This is the Controller for the tab-dash page"
 
 })
 
